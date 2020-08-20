33a2f2fa0415e4b274aec04b1299e9b96f24ca9f
Anca Simon <ancarsimon@gmail.com>
2020-08-20 15:09:57 -0500

    Add project files.

---
+++ FavoritePizzaToppings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FavoritePizzaToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var pizzas = new List<string>();
+            //var pizzasJson = JsonSerializer.Serialize(pizzas, new JsonSerializerOptions { WriteIndented = true });
+
+            //Pizza pizza1 = JsonSerializer.Deserialize<pizzas>
+        }
+    }
+}
